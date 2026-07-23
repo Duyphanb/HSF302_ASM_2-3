6fc4c43f88461288a0ff14c86bb4909da0808758
TranGiaBao2005 <trangiabao4461@gmail.com>
2026-07-23 10:59:44 +0700

    rich text in list and Action view

--- src/main/java/com/assignment/asm/controller/FoodController.java
+++ src/main/java/com/assignment/asm/controller/FoodController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -27,5 +28,18 @@ public class FoodController {
         box.addAttribute("foodbags", foods);
 
         return "food/list";
+    }
+
+    @GetMapping("/{id}")
+    public String getFoodDetail(@PathVariable Integer id, Model box) {
+
+        Food food = foodService.getFoodByName(id);
+
+        if (food == null) {
+            return "redirect:/food/donuong";
+        }
+
+        box.addAttribute("food", food);
+        return "food/detail";
     }
 }
